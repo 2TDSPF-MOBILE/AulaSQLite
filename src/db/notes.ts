15ee43bb1737ecabeb08286b48600e848d21f97e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-21 11:18:08 -0300

    feat: initialized the project

---
+++ src/db/notes.ts
@@ -0,0 +1,43 @@
+import * as SQLite from 'expo-sqlite';
+
+//abre ou cria o local database called notas.db
+const db = SQLite.openDatabaseSync("notas.db")
+
+//creating a table if it doesnt exist
+db.execSync(`
+    CREATE TABLE IF NOT EXISTS NOTES(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       title TEXT NOT NULL,
+       content TEXT,
+       createdAt TEXT NOT NULL
+    )`)
+
+
+//func para ler todas as notas do banco de dados
+export function getNotes(){
+    return db.getAllSync("SELECT * FROM notes ORDER BY id DESC ")
+}
+
+//func para adicionar uma nova nota
+export function addNotes(title: string, content: string){
+    const createdAt = new Date().toISOString()
+    db.runSync(
+          "INSERT INTO notes (title, content, createdAt) VALUES (?,?,?)",
+          [title, content, createdAt]
+    )
+}
+
+//func para atualizar a nota
+export function updatedNote(id:number, title: string, content: string){
+    db.runSync('UPDATE notes SET title=?, content=? WHERE id=?',
+        [title, content, id]
+    )
+}
+
+//func para deletar a nota pelo id
+export function deleteNote(id:number){
+    db.runSync("DELETE FROM notes WHERE id=?",
+        [id]
+        )
+
+}
