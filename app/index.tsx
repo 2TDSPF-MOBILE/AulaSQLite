3c87da9faa77358eae5b5cc6ab7edeb0b6b05639
juvascoli <jvascoli6@gmail.com>
2025-10-21 11:22:38 -0300

    feat: initialized the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+
+  const router =useRouter()
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Button title="Adicionar nota" onPress={()=>router.push("/add")}></Button>
+      <Text>LISTA DE NOTASS</Text>
     </View>
+      
   );
 }
